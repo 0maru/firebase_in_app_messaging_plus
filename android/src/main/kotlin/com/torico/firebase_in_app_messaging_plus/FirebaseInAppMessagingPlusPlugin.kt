2a9248891fd91bdfe9351ca40db939ec79b6cfa6
0maru <0maru.dev@gmail.com>
2021-11-14 23:24:26 +0900

    android のコールバック書いた

--- android/src/main/kotlin/com/torico/firebase_in_app_messaging_plus/FirebaseInAppMessagingPlusPlugin.kt
+++ android/src/main/kotlin/com/torico/firebase_in_app_messaging_plus/FirebaseInAppMessagingPlusPlugin.kt
@@ -1,35 +1,61 @@
 package com.torico.firebase_in_app_messaging_plus
 
 import androidx.annotation.NonNull
+import com.google.firebase.inappmessaging.FirebaseInAppMessaging
+import com.google.firebase.inappmessaging.FirebaseInAppMessagingClickListener
+import com.google.firebase.inappmessaging.model.Action
+import com.google.firebase.inappmessaging.model.InAppMessage
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
 /** FirebaseInAppMessagingPlusPlugin */
-class FirebaseInAppMessagingPlusPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FirebaseInAppMessagingPlusPlugin : FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "firebase_in_app_messaging_plus")
-    channel.setMethodCallHandler(this)
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "torico/firebase_in_app_messaging_plus")
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+        FirebaseInAppMessaging.getInstance().addClickListener(FIAMClickListener(channel))
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        TODO("Not yet implemented")
+    }
+}
+
+class FIAMClickListener(channel: MethodChannel) : FirebaseInAppMessagingClickListener {
+    private var _channel = channel
+    override fun messageClicked(inAppMessage: InAppMessage, action: Action) {
+        println(inAppMessage.data)
+        println(action)
+        if (inAppMessage.campaignMetadata != null) {
+            println(inAppMessage.campaignMetadata?.campaignName)
+            println(inAppMessage.campaignMetadata?.campaignId)
+            val campaignName: String? = inAppMessage.campaignMetadata?.campaignName
+            val campaignId: String? = inAppMessage.campaignMetadata?.campaignId
+            val campaignInfo: Map<String, String?> = hashMapOf(
+                    "campaignName" to campaignName,
+                    "campaignId" to campaignId
+            )
+            val appData: Map<String, Any?> = hashMapOf(
+                    "appData" to inAppMessage.data
+            )
+            val params: Map<Any, Any> = hashMapOf(
+                    "campaignInfo" to campaignInfo,
+                    "appData" to appData
+            )
+            _channel.invokeMethod("onMessageSuccess", params)
+        } else {
+            val params: Map<Any, Any> = hashMapOf()
+            _channel.invokeMethod("onMessageError", params)
+        }
+    }
 }
